2fbab0f2cc8262f8df8392908e0a14ce643f7edf
Andrew Kim <email": null>
2016-03-15 13:50:43 -0400

    forgot to add the file to commit

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
-  $("h4 span").on("click","a", function(e){
+  $("h4 a").on("click", function(e){
     e.preventDefault();
+    // debugger
+      data = $(this)
 
     $.ajax({
       method:"POST",
       url: $(event.target).attr("href"),
-      data:""
+      data:data.data()
     }).done(function(response){
-      debugger
+      data.hide();
     }).fail(function(response){
       debugger
     })
